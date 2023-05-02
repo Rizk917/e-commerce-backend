1f56583d7fc768811a31ff54f4418b7095086d43
Rizk917 <117639834+Rizk917@users.noreply.github.com>
2023-05-02 19:31:26 +0300

    popularCard

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import aboutRoute from "./routes/aboutRoute.js";
 import bodyParser from 'body-parser';
 import errorHandler from './middleware/error.middleware.js';
 import cartRoutes from "./routes/cartRoutes.js"
+import popularRoutes from "./routes/popularProductRoute.js"
 import orderRoutes from './routes/orderRoutes.js'
 import imageCarouselRoute from "./routes/imageCarousel.route.js";
 import cors from "cors";
@@ -46,6 +47,7 @@ app.use('/order', orderRoutes);
 app.use('/cart', cartRoutes)
 app.use("/imageCarousel", imageCarouselRoute);
 app.use("/about",aboutRoute)
+app.use("/popular",popularRoutes)
 
 
 
